587274976aa6e728efdaa8382a873375083ebfa2
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-08-24 15:11:08 +0545

    remember me option added to loginActivity

--- app/src/main/java/com/example/taskmanager/LoginActivity.java
+++ app/src/main/java/com/example/taskmanager/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.taskmanager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText username;
     EditText password;
+    CheckBox chkLoggedIn;
     Button loginButton;
 
     @Override
@@ -32,9 +37,21 @@ public class LoginActivity extends AppCompatActivity {
 
         username = findViewById(R.id.userID);
         password = findViewById(R.id.userPassword);
+        chkLoggedIn = findViewById(R.id.chkLoggedIn);
         loginButton = findViewById(R.id.loginBtn);
 
         Intent iHome = new Intent(LoginActivity.this, MainActivity.class);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("remember", "");
+
+        if (checkbox.equals("true")) {
+            Toast.makeText(LoginActivity.this, "Welcome to Home Page", Toast.LENGTH_SHORT).show();
+            startActivity(iHome);
+            finish();
+        } else {
+            Toast.makeText(LoginActivity.this, "Please! Log In", Toast.LENGTH_SHORT).show();
+        }
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +68,23 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Please!! fill username and password", Toast.LENGTH_SHORT).show();
                 }
 
+            }
+        });
+
+        chkLoggedIn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "true");
+                    editor.apply();
+                } else {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+                }
             }
         });
 
